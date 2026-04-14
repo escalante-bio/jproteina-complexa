bbe01b7a621f2620e2720e77c949a788ba543ad9
nboyd <nickboyd@gmail.com>
2026-04-14 18:14:38 +0000

    fix: restore SD to canonical atom37 index 18 in ATOM_NAMES
    SD (methionine sulfur) was misplaced at index 36 (end of list) instead
    of index 18, shifting all atoms 18-35 by -1 relative to the OpenFold
    canonical ordering. RESTYPE_ATOM37_MASK and CHI_ATOM_INDICES both use
    OpenFold's ordering, so the mismatch caused wrong atom names in PDB I/O
    for 9 of 20 residues (ARG, GLN, GLU, HIS, LYS, MET, PHE, TRP, TYR).
    
    Fixes escalante-bio/mosaic#60

--- src/jproteina_complexa/constants.py
+++ src/jproteina_complexa/constants.py
@@ -17,9 +17,9 @@ AA_1TO_IDX = {aa: i for i, aa in enumerate(AA_CODES)}
 
 ATOM_NAMES = [
     "N", "CA", "C", "CB", "O", "CG", "CG1", "CG2", "OG", "OG1", "SG",
-    "CD", "CD1", "CD2", "ND1", "ND2", "OD1", "OD2", "CE", "CE1", "CE2",
-    "CE3", "NE", "NE1", "NE2", "OE1", "OE2", "CH2", "NH1", "NH2", "OH",
-    "CZ", "CZ2", "CZ3", "NZ", "OXT", "SD",
+    "CD", "CD1", "CD2", "ND1", "ND2", "OD1", "OD2", "SD", "CE", "CE1",
+    "CE2", "CE3", "NE", "NE1", "NE2", "OE1", "OE2", "CH2", "NH1", "NH2",
+    "OH", "CZ", "CZ2", "CZ3", "NZ", "OXT",
 ]
 
 # ---- Residue-atom mask ----
